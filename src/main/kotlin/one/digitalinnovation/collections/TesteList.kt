531927b62de37098983127601f9eab2f5ec3e279
Filipe <filipe_braz@hotmail.com>
2021-08-02 23:32:11 -0300

    created group by Tipo contratação

--- src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitalinnovation.collections
 
 fun main() {
-    val filipe = Funcionario("Filipe", 2500.00)
-    val maria = Funcionario("Maria", 2800.00)
-    val carlos = Funcionario("Carlos", 1600.00)
+    val filipe = Funcionario("Filipe", 2500.00, "CLT")
+    val maria = Funcionario("Maria", 2800.00, "PJ")
+    val carlos = Funcionario("Carlos", 1600.00, "CLT")
 
     val funcionarios = listOf(filipe, maria, carlos)
 
@@ -16,11 +16,17 @@ fun main() {
 
     funcionarios.sortedBy { it.salario }
         .forEach{println(it)}
+
+    println("==========================================================")
+
+    funcionarios.groupBy { it.tipoContratacao }
+        .forEach{println(it)}
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ){
     override fun toString(): String =
             """
